12f979dd6ab99342f05c057b6095af3f236c9ab0
algernonking <algernonking@172.16.160.47>
2017-11-03 15:21:43 +0800

    ok

--- src/main/java/com/dt/core/common/annotion/Acl.java
+++ src/main/java/com/dt/core/common/annotion/Acl.java
@@ -9,13 +9,14 @@ import java.lang.annotation.Target;
 /**
  * @author: algernonking
  * @date: 2017年8月6日 下午3:22:48
- * @Description: Acl必须要设置,否则不能访问
+ * @Description: Acl必须要设置,否则不能访问,allow:无需认证就可访问，common:登录后就可以访问,deny:不可以访问
  */
 @Documented
 @Target({ ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Acl {
 	public static String TYPE_ALLOW = "allow";
+	public static String TYPE_USER_COMMON= "common";
 	public static String TYPE_DENY = "deny";
 
 	String value() default TYPE_DENY;
