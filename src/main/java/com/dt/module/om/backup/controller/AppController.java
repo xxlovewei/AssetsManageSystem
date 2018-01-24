0b0578010acc090552beeb275e239ea5c9f6aeed
algernonking <algernonking@172.16.160.168>
2018-01-24 17:32:34 +0800

    ok

---
+++ src/main/java/com/dt/module/om/backup/controller/AppController.java
@@ -0,0 +1,18 @@
+package com.dt.module.om.backup.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dt.core.common.base.BaseController;
+
+/** 
+ * @author: algernonking
+ * @date: 2018年1月24日 下午5:25:46 
+ * @Description: TODO 
+ */
+@Controller
+@RequestMapping("/api")
+public class AppController extends BaseController{
+
+}
+
---
+++ src/main/java/com/dt/module/om/backup/service/AppService.java
@@ -0,0 +1,49 @@
+package com.dt.module.om.backup.service;
+
+import org.springframework.stereotype.Service;
+
+import com.dt.core.common.annotion.impl.ResData;
+import com.dt.core.common.base.BaseService;
+import com.dt.core.common.util.support.TypedHashMap;
+
+/**
+ * @author: algernonking
+ * @date: 2018年1月24日 下午5:25:24
+ * @Description: TODO
+ */
+@Service
+public class AppService extends BaseService {
+
+	public ResData addApp(TypedHashMap<String, Object> ps) {
+		return null;
+	}
+
+	public ResData queryAppById(TypedHashMap<String, Object> ps) {
+		return null;
+	}
+
+	public ResData queryApp(TypedHashMap<String, Object> ps) {
+		return null;
+	}
+
+	public ResData deleteApp(TypedHashMap<String, Object> ps) {
+		return null;
+	}
+
+	public ResData update(TypedHashMap<String, Object> ps) {
+		return null;
+	}
+
+	public ResData appTypeQuery(TypedHashMap<String, Object> ps) {
+		return null;
+	}
+
+	public ResData appRecQueryById(TypedHashMap<String, Object> ps) {
+		return null;
+	}
+
+	public ResData appRecQuery(TypedHashMap<String, Object> ps) {
+		return null;
+	}
+
+}
