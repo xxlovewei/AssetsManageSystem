21f5475c64024a7c1cbcd0d06d7d0dfbe216c6c6
Lank <maillank@qq.com>
2018-02-10 16:07:33 +0800

    no commit message

--- src/main/java/com/dt/module/om/backup/controller/AppController.java
+++ src/main/java/com/dt/module/om/backup/controller/AppController.java
@@ -1,6 +1,7 @@
 package com.dt.module.om.backup.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.dt.core.annotion.Acl;
@@ -20,9 +21,11 @@ public class AppController extends BaseController {
 	@RequestMapping("/test/test.do")
 	@Res
 	@Acl(value = Acl.TYPE_ALLOW, info = "启动应用节点")
-	public String startNodeApp(String id) {
+	public String startNodeApp(Model model, String id) {
 
-	 
+		System.out.println("id:" + id);
+		model.addAttribute("name", "Dear");
+
 		return "zoolist";
 	}
 
