56d8171bd153801637584211bd59d8f30fa7b67f
Lank <maillank@qq.com>
2018-02-28 09:32:07 +0800

    no commit message

--- src/main/java/com/dt/core/common/base/BaseService.java
+++ src/main/java/com/dt/core/common/base/BaseService.java
@@ -1,10 +1,50 @@
 package com.dt.core.common.base;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.dt.core.tool.util.support.HttpKit;
+
 /**
  * @author: algernonking
  * @date: 2017年8月3日 上午6:27:47
  * @Description: TODO
  */
 public class BaseService extends BaseSC {
-	
+
+	private static Logger _log = LoggerFactory.getLogger(BaseService.class);
+
+	protected HttpServletRequest getHttpServletRequest() {
+		return HttpKit.getRequest();
+	}
+
+	protected HttpServletResponse getHttpServletResponse() {
+		return HttpKit.getResponse();
+	}
+
+	protected HttpSession getSession() {
+		return HttpKit.getRequest().getSession();
+	}
+
+	protected HttpSession getSession(Boolean flag) {
+		return HttpKit.getRequest().getSession(flag);
+	}
+
+	protected String getPara(String name) {
+		return HttpKit.getRequest().getParameter(name);
+	}
+
+	protected void setAttr(String name, Object value) {
+		HttpKit.getRequest().setAttribute(name, value);
+	}
+
+	public String getUserId() {
+		String user_id = (String) HttpKit.getRequest().getSession().getAttribute("user_id");
+		_log.info("getUserId:" + user_id);
+		return user_id;
+	}
 }
