a377ae03f9f3872cd41bbf8aa9ea1e03315e5255
Lank <maillank@qq.com>
2018-03-16 16:18:12 +0800

    no commit message

--- src/main/java/com/dt/core/common/base/BaseConstants.java
+++ src/main/java/com/dt/core/common/base/BaseConstants.java
@@ -5,7 +5,6 @@ package com.dt.core.common.base;
  * @version 创建时间：2017年8月1日 上午8:33:12 全局常量
  */
 public class BaseConstants {
-
 	public static String shiroenable = "true";
 	public static String superadmin = "sys";
 
