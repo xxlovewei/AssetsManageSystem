f260ac219db492fa1d872cb90078a6355f81cb85
Lank <maillank@qq.com>
2018-03-19 16:59:22 +0800

    no commit message

--- src/main/java/com/dt/module/db/DB.java
+++ src/main/java/com/dt/module/db/DB.java
@@ -13,7 +13,7 @@ import com.dt.core.tool.lang.SpringContextUtil;
 
 @Component
 @Scope(BeanDefinition.SCOPE_SINGLETON)
-public class DB extends SpringOracleDao {
+public class DB extends SpringMySQLDao {
 	private static Logger _log = LoggerFactory.getLogger(DB.class);
 
 	public static DB instance() {
