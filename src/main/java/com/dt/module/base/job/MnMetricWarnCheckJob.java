4cedc7471a604df5ca6fdeb483e676dad2789e4e
algernonking <algernonking@lankpc>
2018-04-12 07:25:12 +0800

    ok

---
+++ src/main/java/com/dt/module/base/job/MnMetricWarnCheckJob.java
@@ -0,0 +1,21 @@
+package com.dt.module.base.job;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import com.dt.module.base.schedule.service.JobService;
+import com.dt.module.om.service.WarnService;
+
+/**
+ * @author: algernonking
+ * @date: 2017年11月9日 下午2:30:20
+ * @Description: TODO
+ */
+public class MnMetricWarnCheckJob implements Job {
+	@Override
+	public void execute(JobExecutionContext jc) throws JobExecutionException {
+
+		WarnService.me().queryWarnMetric();
+		JobService.me().finishedJobUpdate(jc);
+	}
+}
