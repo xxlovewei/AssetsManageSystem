21d0307fba711746254d13fce9bdfedbf31ee9ab
algernonking <algernonking@172.16.160.120>
2018-04-24 10:07:24 +0800

    ok

---
+++ src/main/java/com/dt/core/tool/enums/Snippet.java
@@ -0,0 +1,20 @@
+package com.dt.core.tool.enums;
+
+/** 
+ * @author: algernonking
+ * @date: 2018年4月22日 上午11:26:24 
+ * @Description: TODO 
+ */
+public class Snippet {
+	/** 
+	 * @Title: MISSING 
+	 * @Description: TODO
+	 * @return: void
+	 */
+	public static void main(String[] args) {
+	 
+	}
+}
+
+
+
