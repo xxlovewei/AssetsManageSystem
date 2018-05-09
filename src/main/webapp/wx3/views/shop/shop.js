341c626fc5dbe51b7e4cc6e9c4fd5e3d0ce6baec
Lank <maillank@qq.com>
2018-05-09 09:27:52 +0800

    no commit message

---
+++ src/main/webapp/wx3/views/shop/shop.js
@@ -0,0 +1,11 @@
+function shopCtl($log, $http, $scope, $rootScope, $state) {
+	 
+	$rootScope.footshow = true;
+	$rootScope.footcurrent = "shop";
+
+	
+	
+	
+}
+
+app.register.controller('shopCtl', shopCtl);
