64f8d9e2703eca877beaa0a48e82912381df5111
Lank <maillank@qq.com>
2018-05-10 08:27:58 +0800

    no commit message

--- src/main/webapp/wx/js/app_main.js
+++ src/main/webapp/wx/js/app_main.js
@@ -1,10 +1,30 @@
-var app = angular.module('app', [ 'ui.router', 'oc.lazyLoad', 'angular-loading-bar','ksSwiper' ])
+var app = angular.module('app', ['ui.router', 'oc.lazyLoad',
+				'angular-loading-bar', 'ksSwiper'])
 var $injector = angular.injector();
 
+function pageTitle($rootScope, $timeout, $transitions) {
+	return {
+		link : function(scope, element) {
+			$transitions.onSuccess({
+						to : '**'
+					}, function(trans) {
+						console.log("set title.");
+						var current = trans.router.globals.current;
+						var title = "无";
+						if (angular.isDefined(current.data)
+								&& angular.isDefined(current.data.pageTitle)) {
+							title = current.data.pageTitle;
+						}
+						element.text(title);
+					});
+		}
+	}
+};
 var version = "20170906";
 
-function config_main(cfpLoadingBarProvider, $locationProvider, $controllerProvider, $compileProvider, $stateProvider, $filterProvider, $provide, $urlRouterProvider,
-		$ocLazyLoadProvider, $httpProvider) {
+function config_main(cfpLoadingBarProvider, $locationProvider,
+		$controllerProvider, $compileProvider, $stateProvider, $filterProvider,
+		$provide, $urlRouterProvider, $ocLazyLoadProvider, $httpProvider) {
 	// 圈圈延迟出现控制
 	console.log("App main config");
 	cfpLoadingBarProvider.latencyThreshold = 1500;
@@ -20,8 +40,8 @@ function config_main(cfpLoadingBarProvider, $locationProvider, $controllerProvid
 
 	$urlRouterProvider.otherwise("/me/profile");
 	$ocLazyLoadProvider.config({
-		debug : true
-	});
+				debug : true
+			});
 	$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
 	// $httpProvider.defaults.withCredentials = false;
 	var param = function(obj) {
@@ -45,15 +65,18 @@ function config_main(cfpLoadingBarProvider, $locationProvider, $controllerProvid
 					query += param(innerObj) + '&';
 				}
 			} else if (value !== undefined && value !== null)
-				query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+				query += encodeURIComponent(name) + '='
+						+ encodeURIComponent(value) + '&';
 		}
 
 		return query.length ? query.substr(0, query.length - 1) : query;
 	};
 
-	$httpProvider.defaults.transformRequest = [ function(data) {
-		return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
-	} ];
+	$httpProvider.defaults.transformRequest = [function(data) {
+		return angular.isObject(data) && String(data) !== '[object File]'
+				? param(data)
+				: data;
+	}];
 	// // 登录
 	// $stateProvider.state('login', {
 	// url : "/login",
@@ -79,36 +102,39 @@ function config_main(cfpLoadingBarProvider, $locationProvider, $controllerProvid
 	// })
 }
 
-app.config(config_main).run(function($rootScope, $state, $http, $log, $transitions, $templateCache) {
-	console.log("App main run");
-	// 替换了之前的$stateNotFound
-	$state.onInvalid(function(to, from, injector) {
-		$log.warn(to);
-		$log.warn(from);
-		$log.warn(injector);
-		alert("未配置路由");
-	});
-	// 替换了之前的$stateChangeStart
-	$transitions.onStart({
-		to : '**'
-	}, function(trans) {
-		// 调试阶段去除缓存
-		var $state = trans.router.stateService;
-		var from_arr = trans._treeChanges.from;
-		var from = null;
-		if (from_arr.length > 0) {
-			from = from_arr[from_arr.length - 1].state.name;
-		}
+app.config(config_main)
+		.run(
+				function($rootScope, $state, $http, $log, $transitions,
+						$templateCache) {
+					console.log("App main run");
+					// 替换了之前的$stateNotFound
+					$state.onInvalid(function(to, from, injector) {
+								$log.warn(to);
+								$log.warn(from);
+								$log.warn(injector);
+								alert("未配置路由");
+							});
+					// 替换了之前的$stateChangeStart
+					$transitions.onStart({
+								to : '**'
+							}, function(trans) {
+								// 调试阶段去除缓存
+								var $state = trans.router.stateService;
+								var from_arr = trans._treeChanges.from;
+								var from = null;
+								if (from_arr.length > 0) {
+									from = from_arr[from_arr.length - 1].state.name;
+								}
 
-		$log.warn("from:", from);
+								$log.warn("from:", from);
 
-	});
-	$rootScope.$state = $state;
-	$rootScope.project = '/dt/';
-	$rootScope.version = '20170901';
+							});
+					$rootScope.$state = $state;
+					$rootScope.project = '/dt/';
+					$rootScope.version = '20170901';
 
-});
+				});
 
 app.config(config_shop).run(function() {
-	console.log("App Shop run");
-});
+			console.log("App Shop run");
+		});
--- src/main/webapp/wx/js/controllers.js
+++ src/main/webapp/wx/js/controllers.js
@@ -1,27 +1,4 @@
-function MainCtrl($log, $http, $scope, $rootScope, $state,$window) {
-
-	/* 底部固定条 */
-	$rootScope.footData = [ {
-		id : "index",
-		href : "index.ct",
-		image : "mui-icon mui-icon-home",
-		name : "首页"
-	}, {
-		id : "cat",
-		href : "cat.ct",
-		image : "mui-icon-extra mui-icon-extra-class",
-		name : "分类"
-	}, {
-		id : "shop",
-		href : "shop.ct",
-		image : "mui-icon-extra mui-icon-extra-cart",
-		name : "购物车"
-	}, {
-		id : "me",
-		href : "me.profile",
-		image : "mui-icon mui-icon-contact",
-		name : "我"
-	} ];
+function MainCtrl($log, $http, $scope, $rootScope, $state, $window) {
 
 	$rootScope.footshow = true
 	$scope.footerClick = function(item) {
@@ -41,9 +18,9 @@ function MainCtrl($log, $http, $scope, $rootScope, $state,$window) {
 	}
 
 	$scope.goIndex = function() {
-		 
+
 		//$window.open("/dt/wx/loc.html", '_blank');
-		 $state.go("index.ct");
+		$state.go("index.ct");
 	}
 
 	$scope.goBack = function() {
