e2445fe58e8280b0014c46586881d9060090bc15
algernonking <algernonking@192.168.31.10>
2018-05-15 22:15:38 +0800

    ok

--- src/main/webapp/wx/views/me/profile.js
+++ src/main/webapp/wx/views/me/profile.js
@@ -1,7 +1,80 @@
 function meCtl($log, $http, $scope, $rootScope, $state) {
 
-	$rootScope.footcurrent = "me";
+	function isElementInViewport (el, offset = 0) {
+        const box = el.getBoundingClientRect(),
+              top = (box.top >= 0),
+              left = (box.left >= 0),
+              bottom = (box.bottom <= (window.innerHeight || document.documentElement.clientHeight) + offset),
+              right = (box.right <= (window.innerWidth || document.documentElement.clientWidth) + offset);
+        return (top && left && bottom && right);
+    }
+	
+	
+	$scope.item={
+			dizhi:"",
+			xingming:"王可可",
+			rq:"",
+			shouji:"15728001528",
+			zone:"浙江省-宁波-海曙"
+	}
+	$scope.selZone=function(){
+		console.log('selZone');
+		
+	}
+	$scope.selRq=function(){
+		console.log('selRq');	
+	}
+ 
+	$scope.sure = function() {
+		// 如果有数据,可以触发提交
+		if(angular.isDefined($scope.item.dizhi)&&$scope.item.dizhi.length>0){
+			toYY();
+			return;
+		}
+		var box=document.getElementById('firstel');
+		var boxTop=box.getBoundingClientRect().top;
+		var isshow=isElementInViewport(box);
+		console.log(isshow);
+		// 可见
+		if(isshow||boxTop<=0){
+			console.log("can see")
+			toYY();
+		}else{
+			 $('html, body').animate({  
+                 scrollTop: $("#firstel").offset().top  
+             }, 800);  
+		}
+	}
+ 
+	function toYY(){
 
+		if(!(angular.isDefined($scope.item.dizhi)&&$scope.item.dizhi.length>0)){
+			alert("请输入地址");
+			return;
+		}
+		if(!(angular.isDefined($scope.item.shouji)&&$scope.item.shouji.length>0)){
+			alert("请输入手机");
+			return;
+		}
+		if(!(angular.isDefined($scope.item.xingming)&&$scope.item.xingming.length>0)){
+			alert("请输入姓名");
+			return;
+		}
+		if(!(angular.isDefined($scope.item.rq)&&$scope.item.rq.length>0)){
+			alert("请选择日期");
+			return;
+		}
+		if(!(angular.isDefined($scope.item.chengs)&&$scope.item.chengs.length>0)){
+			alert("请选择区域");
+			return;
+		}
+		console.log('ToYY');
+	}
+	
+	
+	
+	$rootScope.footcurrent = "me";
+	$rootScope.footshow = false;
 	$scope.ltdata = [ {
 		ltuser_name : "人员",
 		code : "1212",
