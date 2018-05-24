11d891a7d1b3e8831c7ad08d53fc2b5185c28fcb
algernonking <algernonking@172.16.160.75>
2018-05-25 07:59:59 +0800

    ok

--- src/main/java/com/dt/module/base/controller/FundController.java
+++ src/main/java/com/dt/module/base/controller/FundController.java
@@ -1,13 +1,8 @@
 package com.dt.module.base.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import com.dt.core.annotion.Acl;
 import com.dt.core.common.base.BaseController;
-import com.dt.core.common.base.R;
-import com.dt.module.base.service.FundService;
 
 /**
  * @author: algernonking
@@ -17,24 +12,5 @@ import com.dt.module.base.service.FundService;
 @Controller
 @RequestMapping(value = "/api")
 public class FundController extends BaseController {
-
-	@Autowired
-	FundService fundService;
-
-	@ResponseBody
-	@Acl(info = "查询我到提现记录", value = Acl.ACL_USER)
-	@RequestMapping(value = "/user/queryMyFundTix.do")
-	public R queryMyFundTix() {
-		return fundService.queryMyFundTix(null);
-	}
-
-	@ResponseBody
-	@Acl(info = "查询我的资金变动情况", value = Acl.ACL_USER)
-	@RequestMapping(value = "/user/queryMyFundChange.do")
-	public R queryMyFundChange() {
-
-		return fundService.queryMyFundChange();
-
-	}
 
 }
