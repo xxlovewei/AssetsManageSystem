0c580c4ab43613120baef3aac66f22b8e6d88206
algernonking <algernonking@172.16.160.75>
2018-07-06 22:21:52 +0800

    ok

--- src/main/webapp/console/clearmodules.sh
+++ src/main/webapp/console/clearmodules.sh
@@ -6,10 +6,23 @@ rm -rf datatables.net
 rm -rf datatables.net-dt
 rm -rf "@uirouter"
 find ./ -type d -name "node_modules" -exec rm -rf {} \;
+find ./ -type d -name "bower_components" -exec rm -rf {} \;
+find ./ -type d -name "coverage" -exec rm -rf {} \;
 find ./ -type d -name "demo" -exec rm -rf {} \;
 find ./ -type d -name "src" -exec rm -rf {} \;
 find ./ -type d -name "test" -exec rm -rf {} \;
-
-
-
+find ./ -type d -name "less" -exec rm -rf {} \;
+find ./ -type d -name "docs" -exec rm -rf {} \;
+find ./ -type d -name "swf" -exec rm -rf {} \;
+find ./ -type d -name "example" -exec rm -rf {} \;
+find ./  -name "*.html" -exec rm -rf {} \;
+find ./  -name "*.md" -exec rm -rf {} \;
+find ./  -name "yarn.lock" -exec rm -rf {} \;
+find ./  -name "*.sh" -exec rm -rf {} \;
+find ./  -name "*.txt" -exec rm -rf {} \;
+find ./  -name "package.json" -exec rm -rf {} \;
+find ./  -name "*LICENSE*" -exec rm -rf {} \;
+find ./  -name "*runtfile.js" -exec rm -rf {} \;
+find ./  -name "gulpfile.js" -exec rm -rf {} \;
+find ./  -name "bower.json" -exec rm -rf {} \;
 sleep 5
