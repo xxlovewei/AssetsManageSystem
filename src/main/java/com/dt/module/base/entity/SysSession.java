0395841f0aae244bdca5d611e25ad699f2a8d027
algernonking <algernonking@192.168.31.10>
2018-07-26 14:58:00 +0800

    ok

---
+++ src/main/java/com/dt/module/base/entity/SysSession.java
@@ -0,0 +1,167 @@
+package com.dt.module.base.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import java.util.Date;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableField;
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author algernonking
+ * @since 2018-07-26
+ */
+@TableName("SYS_SESSION")
+public class SysSession extends Model<SysSession> {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableField("TOKEN")
+    private String token;
+    @TableField("LASTACCESS")
+    private Date lastaccess;
+    @TableField("EXPIRE")
+    private String expire;
+    @TableId("COOKIE")
+    private String cookie;
+    @TableField("DTSESSION")
+    private String dtsession;
+    @TableField("ID")
+    private String id;
+    @TableField("USER_ID")
+    private String userId;
+    @TableField("START_TIME")
+    private String startTime;
+    @TableField("LOGIN_TIME")
+    private Date loginTime;
+    @TableField("IP")
+    private String ip;
+    @TableField("AGENT")
+    private String agent;
+    @TableField("CLIENT")
+    private String client;
+
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Date getLastaccess() {
+        return lastaccess;
+    }
+
+    public void setLastaccess(Date lastaccess) {
+        this.lastaccess = lastaccess;
+    }
+
+    public String getExpire() {
+        return expire;
+    }
+
+    public void setExpire(String expire) {
+        this.expire = expire;
+    }
+
+    public String getCookie() {
+        return cookie;
+    }
+
+    public void setCookie(String cookie) {
+        this.cookie = cookie;
+    }
+
+    public String getDtsession() {
+        return dtsession;
+    }
+
+    public void setDtsession(String dtsession) {
+        this.dtsession = dtsession;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getLoginTime() {
+        return loginTime;
+    }
+
+    public void setLoginTime(Date loginTime) {
+        this.loginTime = loginTime;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getAgent() {
+        return agent;
+    }
+
+    public void setAgent(String agent) {
+        this.agent = agent;
+    }
+
+    public String getClient() {
+        return client;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+
+    @Override
+    protected Serializable pkVal() {
+        return this.cookie;
+    }
+
+    @Override
+    public String toString() {
+        return "SysSession{" +
+        ", token=" + token +
+        ", lastaccess=" + lastaccess +
+        ", expire=" + expire +
+        ", cookie=" + cookie +
+        ", dtsession=" + dtsession +
+        ", id=" + id +
+        ", userId=" + userId +
+        ", startTime=" + startTime +
+        ", loginTime=" + loginTime +
+        ", ip=" + ip +
+        ", agent=" + agent +
+        ", client=" + client +
+        "}";
+    }
+}
---
+++ src/main/java/com/dt/module/base/service/impl/SysSessionServiceImpl.java
@@ -0,0 +1,20 @@
+package com.dt.module.base.service.impl;
+
+import com.dt.module.base.entity.SysSession;
+import com.dt.module.base.mapper.SysSessionMapper;
+import com.dt.module.base.service.ISysSessionService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author algernonking
+ * @since 2018-07-26
+ */
+@Service
+public class SysSessionServiceImpl extends ServiceImpl<SysSessionMapper, SysSession> implements ISysSessionService {
+
+}
