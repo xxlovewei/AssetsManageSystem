89d26a1fa6f0e91b7b367764595805168a2cafad
algernonking <algernonking@192.168.31.10>
2018-07-27 20:44:08 +0800

    ok

---
+++ src/main/java/com/dt/module/base/controller/SysCardAddrController.java
@@ -0,0 +1,104 @@
+package com.dt.module.base.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.dt.module.base.entity.SysCardAddr;
+import com.dt.module.base.service.ISysCardAddrService;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.dt.core.annotion.Acl;
+import com.dt.core.common.base.R;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.dt.core.tool.util.DbUtil;
+import com.alibaba.fastjson.JSONObject;
+import com.dt.core.tool.util.ToolUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import org.springframework.stereotype.Controller;
+import com.dt.core.common.base.BaseController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author algernonking
+ * @since 2018-07-27
+ */
+@Controller
+@RequestMapping("/api/sysCardAddr")
+public class SysCardAddrController extends BaseController {
+
+
+	@Autowired
+	ISysCardAddrService SysCardAddrServiceImpl;
+
+
+	@ResponseBody
+	@Acl(info = "根据Id删除", value = Acl.ACL_USER)
+	@RequestMapping(value = "/deleteById.do")
+	public R deleteById(String id) {
+		return R.SUCCESS_OPER(SysCardAddrServiceImpl.deleteById(id));
+	}
+
+	@ResponseBody
+	@Acl(info = "根据Id查询", value = Acl.ACL_USER)
+	@RequestMapping(value = "/selectById.do")
+	public R selectById(String id) {
+		return R.SUCCESS_OPER(SysCardAddrServiceImpl.selectById(id));
+	}
+
+	@ResponseBody
+	@Acl(info = "插入", value = Acl.ACL_USER)
+	@RequestMapping(value = "/insert.do")
+	public R insert(SysCardAddr entity) {
+		return R.SUCCESS_OPER(SysCardAddrServiceImpl.insert(entity));
+	}
+
+	@ResponseBody
+	@Acl(info = "根据Id更新", value = Acl.ACL_USER)
+	@RequestMapping(value = "/updateById.do")
+	public R updateById(SysCardAddr entity) {
+		return R.SUCCESS_OPER(SysCardAddrServiceImpl.updateById(entity));
+	}
+
+	@ResponseBody
+	@Acl(info = "存在则更新,否则插入", value = Acl.ACL_USER)
+	@RequestMapping(value = "/insertOrUpdate.do")
+	public R insertOrUpdate(SysCardAddr entity) {
+		return R.SUCCESS_OPER(SysCardAddrServiceImpl.insertOrUpdate(entity));
+	}
+
+	@ResponseBody
+	@Acl(info = "查询所有,无分页", value = Acl.ACL_USER)
+	@RequestMapping(value = "/selectList.do")
+	public R selectList() {
+		return R.SUCCESS_OPER(SysCardAddrServiceImpl.selectList(null));
+	}
+
+	@ResponseBody
+	@Acl(info = "查询所有,有分页", value = Acl.ACL_USER)
+	@RequestMapping(value = "/selectPage.do")
+	public R selectPage(String start, String length, String pageSize, String pageIndex) {
+		JSONObject respar = DbUtil.formatPageParameter(start, length, pageSize, pageIndex);
+		if (ToolUtil.isEmpty(respar)) {
+			return R.FAILURE_REQ_PARAM_ERROR();
+		}
+		int pagesize = respar.getIntValue("pagesize");
+		int pageindex = respar.getIntValue("pageindex");
+		QueryWrapper<SysCardAddr> ew = new QueryWrapper<SysCardAddr>();
+		//ew.and(i -> i.eq("user_id", getUserId()).apply(pagesize>10, "rtime>sysdate-1","23"));
+		IPage<SysCardAddr> pdata = SysCardAddrServiceImpl.selectPage(new Page<SysCardAddr>(pageindex, pagesize), ew);
+		JSONObject retrunObject = new JSONObject();
+		retrunObject.put("iTotalRecords", pdata.getTotal());
+		retrunObject.put("iTotalDisplayRecords", pdata.getTotal());
+		retrunObject.put("data", JSONArray.parseArray(JSON.toJSONString(pdata.getRecords(),SerializerFeature.WriteDateUseDateFormat, SerializerFeature.DisableCircularReferenceDetect)));
+		return R.clearAttachDirect(retrunObject);
+	}
+
+
+}
+
---
+++ src/main/java/com/dt/module/base/service/ISysCardAddrService.java
@@ -0,0 +1,16 @@
+package com.dt.module.base.service;
+
+import com.dt.module.base.entity.SysCardAddr;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author algernonking
+ * @since 2018-07-27
+ */
+public interface ISysCardAddrService extends IService<SysCardAddr> {
+
+}
