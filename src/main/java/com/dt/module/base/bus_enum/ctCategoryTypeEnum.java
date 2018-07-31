6200936e063485dddbefe6e34c8a53b7e39af3ac
algernonking <algernonking@192.168.31.10>
2018-07-31 08:04:07 +0800

    ok

---
+++ src/main/java/com/dt/module/base/bus_enum/ctCategoryTypeEnum.java
@@ -0,0 +1,28 @@
+package com.dt.module.base.bus_enum;
+
+import java.io.Serializable;
+import com.baomidou.mybatisplus.core.enums.IEnum;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum ctCategoryTypeEnum implements IEnum<Serializable> {
+	SYSTEM("sys", "系统"), EMPL("empl", "组织"), CRM("crm", "会员粉丝"), WX("wx", "微信用户");
+
+	private String code;
+	private String desc;
+
+	ctCategoryTypeEnum(final String code, final String desc) {
+		this.code = code;
+		this.desc = desc;
+	}
+
+	@Override
+	public Serializable getValue() {
+
+		return this.code;
+	}
+
+	@JsonValue
+	public String getDesc() {
+		return this.desc;
+	}
+}
---
+++ src/main/java/com/dt/module/base/bus_enum/ctCtTypeEnum.java
@@ -0,0 +1,12 @@
+package com.dt.module.base.bus_enum;
+
+
+/** 
+ * @author: algernonking
+ * @date: 2018年7月31日 上午7:48:22 
+ * @Description: TODO 
+ */
+public class ctCtTypeEnum {
+
+}
+
