a4cdfc7a935a80a3c070fe4a53a2243c66174f9a
algernonking <algernonking@lankpc>
2019-04-05 23:49:16 +0800

    ok

--- src/main/java/com/dt/module/base/job/CollectApiJob.java
+++ src/main/java/com/dt/module/base/job/CollectApiJob.java
@@ -1,12 +1,31 @@
 package com.dt.module.base.job;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
+import com.dt.core.annotion.Acl;
+import com.dt.core.common.base.R;
+import com.dt.core.dao.sql.Insert;
+import com.dt.core.dao.sql.SQL;
+import com.dt.core.tool.lang.SpringContextUtil;
+import com.dt.core.tool.util.DbUtil;
+import com.dt.core.tool.util.ToolUtil;
 import com.dt.module.base.service.impl.JobService;
+import com.dt.module.db.DB;
 
 /** 
  * @author: algernonking
@@ -19,7 +38,45 @@ public class CollectApiJob implements Job {
 	@Override
 	public void execute(JobExecutionContext jc) throws JobExecutionException {
 		_log.info("collect urls start.");
-		//ApiService.me().updateApi();
+		List<SQL> sqls = new ArrayList<SQL>();
+		WebApplicationContext wc = (WebApplicationContext) SpringContextUtil.getApplicationContext();
+		RequestMappingHandlerMapping bean = wc.getBean(RequestMappingHandlerMapping.class);
+		Map<RequestMappingInfo, HandlerMethod> handlerMethods = bean.getHandlerMethods();
+		for (Map.Entry<RequestMappingInfo, HandlerMethod> entry : handlerMethods.entrySet()) {
+			RequestMappingInfo rmi = entry.getKey();
+			PatternsRequestCondition pc = rmi.getPatternsCondition();
+			Set<String> pSet = pc.getPatterns();
+			HandlerMethod hm = entry.getValue();
+			Acl am = ((HandlerMethod) hm).getMethodAnnotation(Acl.class);
+			if (ToolUtil.isNotEmpty(am)) {
+				String aclvalue = am.value();
+				String aclinfo = am.info();
+				String type = am.type();
+				Iterator<String> it = pSet.iterator();
+				while (it.hasNext()) {
+					String str = it.next();
+					Insert me = new Insert("sys_api");
+					me.set("id", DB.instance().getUUID());
+					me.setIf("ct", str);
+					me.setIf("dr", "0");
+					me.setIf("ctacl", aclvalue);
+					me.setIf("apitype", "url");
+					me.setIf("info", aclinfo);
+					me.setIf("type", type);
+					me.setSE("rectime", DbUtil.getDbDateString(DB.instance().getDBType()));
+					_log.info(str + "," + aclvalue + "," + me.getSQL());
+					sqls.add(me);
+				}
+			}
+
+		}
+		if (sqls.size() > 0) {
+			_log.info("Save collect Api.");
+			DB.instance().tabTruncate("sys_api");
+			DB.instance().executeSQLList(sqls);
+		} else {
+			_log.info("Save collect Api failed.");
+		}
 		JobService.me().finishedJobUpdate(jc);
 	}
 }
