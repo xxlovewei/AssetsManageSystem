f024f00d8f77a0f0234188adb3e9a06eedeb26b4
Lank <maillank@qq.com>
2019-11-06 19:48:40 +0800

    ok

--- src/main/webapp/public/vipspa/example/pure/www/js/app/contact.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-	console.log('联系我们');
-	var param = vipspa.parse();
-	console.log(param);
-	
-});
--- src/main/webapp/public/vipspa/example/pure/www/js/app/content.js
+++
@@ -1,6 +0,0 @@
-
-$(function(){
-	console.log('公司简介');
-	var param = vipspa.getMessage('home_content');
-	console.log(param)
-})
