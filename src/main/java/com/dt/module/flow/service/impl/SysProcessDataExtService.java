b084b09973963535ad0b9e2098ada52615e0b355
Lank <maillank@qq.com>
2019-11-30 19:33:44 +0800

    1

---
+++ src/main/java/com/dt/module/flow/service/impl/SysProcessDataExtService.java
@@ -0,0 +1,18 @@
+package com.dt.module.flow.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.dt.core.common.base.BaseService;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author algernonking
+ * @since 2019-11-30
+ */
+@Service
+public class SysProcessDataExtService extends BaseService{
+
+}
