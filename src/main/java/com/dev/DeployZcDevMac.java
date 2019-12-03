3702086978372210fa729f903cb6102c823719bb
Lank <maillank@qq.com>
2019-12-03 11:23:52 +0800

    更新sql文件

--- src/main/java/com/dev/DeployZcDevMac.java
+++ src/main/java/com/dev/DeployZcDevMac.java
@@ -26,7 +26,7 @@ public class DeployZcDevMac {
 		String rfile = dir + "/" + filename + ".war";
 		String fstr = "/Users/algernonking/" + filename + ".war";
 		
-		String pwd="222";
+		String pwd="22";
 
 		SftpClient sftp = new SftpClient();
 		Machine m = new Machine("localhost", "39.105.191.22", "root", pwd, 12500);
