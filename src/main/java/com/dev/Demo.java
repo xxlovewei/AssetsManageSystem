64d9809d034574f9140c40420128341c5d4beab0
Lank <maillank@qq.com>
2020-04-05 08:13:58 +0800

    12

--- src/main/java/com/dev/Demo.java
+++ src/main/java/com/dev/Demo.java
@@ -6,19 +6,11 @@ import com.alibaba.fastjson.JSONArray;
 
 import cn.hutool.core.util.IdUtil;
 
-/**
- * @author: algernonking
- * @date: Feb 11, 2020 7:55:08 AM
- * @Description: TODO
- */
+
 public class Demo {
 
-	/**
-	 * @Title: main
-	 * @Description: TODO
-	 * @param args
-	 * @return: void
-	 */
+
+
 	public static void main(String[] args) {
 
 	}
